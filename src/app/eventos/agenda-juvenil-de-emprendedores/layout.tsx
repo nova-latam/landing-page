124203befb91242c1410c709b44f30e9b30ed7eb
Alex Padilla <alex.padilla@epn.edu.ec>
2023-10-09 01:44:20 -0500

    feat: events implementation

--- src/app/eventos/agenda-juvenil-de-emprendedores/layout.tsx
+++ src/app/eventos/agenda-juvenil-de-emprendedores/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-
+//Faltan algunos detalles pero si la tendremos lista jajaj https://www.novaclub.tech/eventos/agenda-juvenil-de-emprendedores
 export const metadata: Metadata = {
   title: "Agenda Juvenil de Emprendedores",
   description:
-    "Nova Club es una comunidad de estudiantes apasionados por la innovación y la tecnología. Creemos que la innovación es la clave para resolver los problemas más urgentes del mundo, y estamos comprometidos a desarrollar la próxima generación de innovadores.",
+    "Ven, acompáñanos a este encuentro con el objetivo de fomentar la participación de empresarios juveniles, construiremos una agenda que beneficie a los emprendedores del país",
 };
 
 export default function RootLayout({
