aeabaa6005ed63e3e352ad37c827b2661a015efb
alextintin007 <alextintin007@outlook.com>
2023-10-09 19:43:50 -0500

    Add profile: Xavier Tintin

--- src/components/views/home/team/team.lib.ts
+++ src/components/views/home/team/team.lib.ts
@@ -11,11 +11,11 @@ export const teamMembers: TeamMember[] = [
     },
     {
         name: 'Xavier Tintin',
-        title: 'President',
-        img: '/images/team/alex-padilla.png',
-        bio: 'Innovación y tecnología para el desarrollo sostenible"',
-        instagram: 'https://www.instagram.com/padimaster/',
-        linkedin: 'https://www.linkedin.com/in/hilmalex/',
+        title: 'Vice President',
+        img: '/images/team/xavier-tintin.png',
+        bio: 'Be vacant, and you will remain full',
+        instagram: 'https://www.instagram.com/alextintin_/?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr',
+        linkedin: 'https://www.linkedin.com/in/xavier-tintin/',
     },
     {
         name: 'Marcela Montalvo',
