f215773ee45d64cf79dc9b4f45f1f98d15e1e315
Ricker2020 <enrique_piedra2001@hotmail.com>
2023-11-28 10:39:52 -0500

    none

--- src/components/views/intership/Card.tsx
+++ src/components/views/intership/Card.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export default function Card({information}:any) {
     return (
-        <div className="w-[70%] mx-auto bg-white rounded-xl  shadow-lg p-6 my-10">
+        <div className="w-[80%] mx-auto bg-white rounded-xl  shadow-lg p-6 my-10">
         <div className="mb-4">
           <h2 className="text-xl font-semibold">{information.title}</h2>
           <p className="text-xl font-semibold">{information.place}</p>
