247dae06e6c3635f9c43edba0d024e51d565ec4b
Alex Padilla <0xpadimaster@gmail.com>
2023-12-05 22:08:16 -0500

    Format(pasantias): styles

--- src/app/pasantias/[id]/page.tsx
+++ src/app/pasantias/[id]/page.tsx
@@ -1,15 +1,14 @@
-import Card from "@/components/views/intership/Card"
-import ViewDetails from "@/components/views/intership/Details"
-import { internships } from '@/lib/intership/intership'
-
+import Card from "@/components/views/intership/Card";
+import ViewDetails from "@/components/views/intership/Details";
+import { internships } from "@/lib/intership/intership";
 
 export default function Details({ params }: any) {
-  const { id } = params
-  const item = internships.find((item:any) => item.id == id)
+  const { id } = params;
+  const item = internships.find((item: any) => item.id == id);
 
-
-  return <div className=" text-black my-20 w-[70%] m-auto">
-      <ViewDetails  information={item}/>
-  </div>
-
+  return (
+    <div className=" text-black my-20 w-[70%] m-auto">
+      <ViewDetails information={item} />
+    </div>
+  );
 }
