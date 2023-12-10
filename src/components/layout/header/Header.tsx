fe9380128edd0e3fc1866f9d1b1b16d4a3b7a4b7
Alex Padilla <0xpadimaster@gmail.com>
2023-12-09 22:00:33 -0500

    components/layout/header: fix responsive design

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -7,15 +7,15 @@ import { MobileNav } from "./MobileNav";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 z-50 w-full border-b px-16 py-2 bg-white backdrop-blur">
-      <div className="container flex justify-between h-14 items-center">
+    <header className="fixed top-0 z-50 w-full border-b px-8 md:px-16 py-2 bg-white backdrop-blur">
+      <div className="w-full flex justify-between h-14 items-center">
         <Link href={"/"}>
           <Image
             src={"/images/logos/only-words/light-500x135.png"}
             alt="Hult Prize logo"
             width={200}
             height={100}
-            className="h-8 w-24"
+            className="h-[2rem] w-[6rem] md:h-[2.75rem] md:w-[8.25rem]"
           />
         </Link>
         <MainNav />
